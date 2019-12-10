669f414a30266b08a6d7b24fbfd408812956c692
markvincze <mrk.vincze@gmail.com>
2019-12-11 00:00:03 +0100

    Day 9 F#

--- FSharp/09-sensor.fsx
+++ FSharp/09-sensor.fsx
@@ -18,55 +18,50 @@ type Instruction =
     | AdjustRelativeBase of Parameter
     | Halt
 
-let arrayWith index value array =
-    let result = Array.copy array
-    Array.set result index value
-    result
-
 let numbers = File.ReadAllText("FSharp/09-sensor-input.txt").Split(',')
-              |> Array.map Int32.Parse
+              |> Array.mapi (fun i n -> i, n |> Int64.Parse)
+              |> Array.fold (fun m (i, n) -> Map.add i n m) Map.empty<int, int64>
 
-let modes number = ((number / 100) % 10), ((number / 1000) % 10), ((number / 10000) % 10)
+let modes number = ((number / 100L) % 10L), ((number / 1000L) % 10L), ((number / 10000L) % 10L)
 
-let parameter mode value = match mode with
-                           | 0 -> Position value
-                           | 1 -> Immediate value
-                           | 2 -> Relative value
+let parameter mode (value : int64) = match mode with
+                           | 0L -> Position (int value)
+                           | 1L -> Immediate (int value)
+                           | 2L -> Relative (int value)
                            | _ -> failwith "Invalid input for parameter"
 
-let instruction (numbers : int array) index =
+let instruction (numbers : Map<int, int64>) index =
     let first = numbers.[index]
-    let opcode = first % 100
+    let opcode = first % 100L
     let mode1, mode2, mode3 =  first |> modes
     match opcode with
-    | 1 -> Addition ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
-    | 2 -> Multiplication ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
-    | 3 -> Input (parameter mode1 (numbers.[index+1]))
-    | 4 -> Output (parameter mode1 (numbers.[index+1]))
-    | 5 -> JumpIfTrue ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])))
-    | 6 -> JumpIfFalse ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])))
-    | 7 -> LessThan ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
-    | 8 -> Equals ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
-    | 9 -> AdjustRelativeBase (parameter mode1 (numbers.[index+1]))
-    | 99 -> Halt
+    | 1L -> Addition ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
+    | 2L -> Multiplication ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
+    | 3L -> Input (parameter mode1 (numbers.[index+1]))
+    | 4L -> Output (parameter mode1 (numbers.[index+1]))
+    | 5L -> JumpIfTrue ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])))
+    | 6L -> JumpIfFalse ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])))
+    | 7L -> LessThan ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
+    | 8L -> Equals ((parameter mode1 (numbers.[index+1])), (parameter mode2 (numbers.[index+2])), (parameter mode3 (numbers.[index+3])))
+    | 9L -> AdjustRelativeBase (parameter mode1 (numbers.[index+1]))
+    | 99L -> Halt
     | _ -> failwith "Invalid input for the opcode"
 
-let getValue (numbers : int array) relativeBase parameter =
+let getValue (numbers : Map<int, int64>) relativeBase parameter =
     match parameter with
-    | Position p -> numbers.[p]
-    | Immediate v -> v
-    | Relative p -> numbers.[relativeBase + p]
+    | Position p -> Map.tryFind p numbers |> Option.defaultValue 0L
+    | Immediate v -> v |> int64
+    | Relative p -> Map.tryFind (relativeBase + p) numbers |> Option.defaultValue 0L
 
-let saveValue (numbers : int array) relativeBase position value =
+let saveValue (numbers : Map<int, int64>) relativeBase position value =
     match position with
-    | Position p -> arrayWith p value numbers
+    | Position p -> Map.add p value numbers
     | Immediate _ -> failwith "Invalid input, trying to save at immediate value"
-    | Relative p -> arrayWith (relativeBase + p) value numbers
+    | Relative p -> Map.add (relativeBase + p) value numbers
 
 type ReturnType = InputReadTwice | OutputReturned | Halted
 
-let rec processCode input (numbers : int array) relativeBase index outputs =
-    printfn "ProcessCode called, index: %d, number: %d, relative base: %d" index (numbers.[index]) relativeBase
+let rec processCode input (numbers : Map<int, int64>) relativeBase index outputs =
     match instruction numbers index with
     | Addition (p1, p2, p3) -> let result = (getValue numbers relativeBase p1) + (getValue numbers relativeBase p2)
                                processCode input (saveValue numbers relativeBase p3 result) relativeBase (index + 4) outputs
@@ -75,22 +70,23 @@ let rec processCode input (numbers : int array) relativeBase index outputs =
     | Input p -> match input with
                  | Some input -> processCode None (saveValue numbers relativeBase p input) relativeBase (index + 2) outputs
                  | None -> failwith "Read input twice"
-    | Output p -> //(getValue numbers relativeBase p), numbers, (index + 2), OutputReturned
-                  processCode input numbers relativeBase (index + 2) ((getValue numbers relativeBase p) :: outputs)
-    | JumpIfTrue (p1, p2) -> let newIndex = if (getValue numbers relativeBase p1) <> 0
-                                            then (getValue numbers relativeBase p2)
+    | Output p -> processCode input numbers relativeBase (index + 2) ((getValue numbers relativeBase p) :: outputs)
+    | JumpIfTrue (p1, p2) -> let newIndex = if (getValue numbers relativeBase p1) <> 0L
+                                            then (getValue numbers relativeBase p2) |> int
                                             else (index + 3)
-                             processCode input numbers relativeBase newIndex outputs
-    | JumpIfFalse (p1, p2) -> let newIndex = if (getValue numbers relativeBase p1) = 0
-                                             then (getValue numbers relativeBase p2)
+                             processCode input numbers relativeBase (int newIndex) outputs
+    | JumpIfFalse (p1, p2) -> let newIndex = if (getValue numbers relativeBase p1) = 0L
+                                             then (getValue numbers relativeBase p2) |> int
                                              else (index + 3)
                               processCode input numbers relativeBase newIndex outputs
-    | LessThan (p1, p2, p3) -> let result = if (getValue numbers relativeBase p1) < (getValue numbers relativeBase p2) then 1 else 0
+    | LessThan (p1, p2, p3) -> let result = if (getValue numbers relativeBase p1) < (getValue numbers relativeBase p2) then 1L else 0L
                                processCode input (saveValue numbers relativeBase p3 result) relativeBase (index + 4) outputs
-    | Equals (p1, p2, p3) -> let result = if (getValue numbers relativeBase p1) = (getValue numbers relativeBase p2) then 1 else 0
+    | Equals (p1, p2, p3) -> let result = if (getValue numbers relativeBase p1) = (getValue numbers relativeBase p2) then 1L else 0L
                              processCode input (saveValue numbers relativeBase p3 result) relativeBase (index + 4) outputs
-    | AdjustRelativeBase p -> let relativeBase = relativeBase + (getValue numbers relativeBase p)
+    | AdjustRelativeBase p -> let relativeBase = relativeBase + (getValue numbers relativeBase p |> int)
                               processCode input numbers relativeBase (index + 2) outputs
     | Halt -> (outputs |> List.rev), numbers, (index + 2), Halted
 
-let outputs, _, _, _ = processCode (Some 1) numbers 0 0 []
+let outputs, _, _, _ = processCode (Some 1L) numbers 0 0 []
+
+let outputs2, _, _, _ = processCode (Some 2L) numbers 0 0 []
