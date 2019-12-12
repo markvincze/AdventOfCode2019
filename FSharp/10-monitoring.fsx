0a2fc99a5397bec6d2fee910f81836c1767fba6b
markvincze <mrk.vincze@gmail.com>
2019-12-13 00:58:56 +0100

    Day 10 part 2 F#

--- FSharp/10-monitoring.fsx
+++ FSharp/10-monitoring.fsx
@@ -18,20 +18,34 @@ let rec gcd x y =
     elif x > y then gcd (x-y) y
     else gcd x (y-x)
 
-let raysFrom x y =
-    let rays x y xd yd =
-        if xd = 0
-        then [((+), (+)); ((+), (-))]
-        elif yd = 0
-        then [((+), (+)); ((-), (+))]
-        else [((+), (+)); ((-), (+)); ((+), (-)); ((-), (-))]
-        |> Seq.map (fun (op1, op2) -> Seq.initInfinite (fun i -> op1 x (i * xd), op2 y (i * yd)))
+type Quadrant = TopRight | BottomRight | BottomLeft | TopLeft
 
-    [ 0..(width - 1) ]
-    |> Seq.collect (fun x -> [ for y in 0..(height - 1) -> (x, y)])
+let raysInQuadrant quadrant x y =
+    let orderBy = match quadrant with
+                  | TopRight | BottomLeft -> (fun (x, y) -> if y = 0 then infinity else (float x) / (float y))
+                  | BottomRight | TopLeft -> (fun (x, y) -> if x = 0 then infinity else (float y) / (float x))
+    let op1, op2 = match quadrant with
+                   | TopRight -> (+), (-)
+                   | BottomRight -> (+), (+)
+                   | BottomLeft -> (-), (+)
+                   | TopLeft -> (-), (-)
+    match quadrant with
+    | TopRight | BottomLeft -> [ 0..(width - 1) ]
+                               |> Seq.collect (fun x -> [ for y in 1..(height - 1) -> (x, y)])
+    | BottomRight | TopLeft -> [ 1..(width - 1) ]
+                               |> Seq.collect (fun x -> [ for y in 0..(height - 1) -> (x, y)])
     |> Seq.filter (fun (xd, yd) -> gcd xd yd = 1)
-    |> Seq.collect (fun (xd, yd) -> rays x y xd yd)
-    |> Seq.map (Seq.skip 1 >> Seq.takeWhile inside)
+    |> Seq.sortBy orderBy
+    |> Seq.map (fun (xd, yd) -> Seq.initInfinite (fun i -> op1 x (i * xd), op2 y (i * yd))
+                                |> Seq.skip 1
+                                |> Seq.takeWhile inside)
+
+let raysFrom x y =
+    [ raysInQuadrant TopRight x y
+      raysInQuadrant BottomRight x y
+      raysInQuadrant BottomLeft x y
+      raysInQuadrant TopLeft x y ]
+    |> Seq.concat
 
 let observableCount (asteroids : bool[,]) x y =
     raysFrom x y
@@ -43,3 +57,24 @@ let result1 = [ 0..(width - 1) ]
               |> List.filter (fun (x, y) -> asteroids.[y, x])
               |> List.map (fun (x, y) -> (x, y, (observableCount asteroids x y)))
               |> List.maxBy (fun (_, _, o) -> o)
+
+let baseX, baseY, _ = result1
+
+let rays = raysFrom baseX baseY
+           |> Seq.map List.ofSeq
+           |> List.ofSeq
+           |> List.filter (List.isEmpty >> not) 
+
+let shootRound rays shootCounter =
+    let rec shootRound rays shootCounter newRays =
+        match rays with
+        | [] -> shootCounter, newRays |> List.rev
+        | ray :: rayRest -> let rest = ray |> List.skipWhile (fun (x, y) -> not asteroids.[y, x])
+                            match rest with
+                            | [] -> shootRound rayRest shootCounter newRays
+                            | (x, y) :: t ->
+                                printfn "Shooting %d. asteroid at %d,%d" (shootCounter + 1) x y
+                                asteroids.[y, x] <- false
+                                shootRound rayRest (shootCounter + 1) (t :: newRays)
+
+    shootRound rays shootCounter []
